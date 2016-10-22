910c166f5974c02b0b2ccbcc6da25a76f777b774
Maricarmen Campos <emomari_94@hotmail.com>
2016-10-22 12:36:02 -0500

    Programa Codigo Braile
    Traductor Codigo Braile

---
+++ BrailePrueba1/src/braileprueba1/BrailePrueba1.java
@@ -0,0 +1,14 @@
+package braileprueba1;
+/**
+ *
+ * @author WARY
+ */
+public class BrailePrueba1 {
+
+    
+    public static void main(String[] args) {
+        VentanaBraile abrir = new VentanaBraile();
+        abrir.setVisible(true);
+    }
+    
+}
---
+++ BrailePrueba1/src/braileprueba1/IBrailee.java
@@ -0,0 +1,13 @@
+package braileprueba1;
+
+/**
+ *
+ * @author WARY
+ */
+public interface IBrailee {
+    void setCadenab(String c);
+    String getCadenab();
+    char[] getLetras();
+    void imprimirPdf();
+    
+}
